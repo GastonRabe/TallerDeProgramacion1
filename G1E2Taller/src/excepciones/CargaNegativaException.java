b4cc0e4b0baca34ee57185bc0c7aaf1f17c78d0e
Valentín <89087929+ValentinGarciaIriart@users.noreply.github.com>
2021-09-01 14:19:07 -0300

    Caso Surtidor

---
+++ G1E2Taller/src/excepciones/CargaNegativaException.java
@@ -0,0 +1,11 @@
+package excepciones;
+
+public class CargaNegativaException extends Exception {
+
+	public CargaNegativaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
